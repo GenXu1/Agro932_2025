986cd1e3ab36b9c2d1899e6a84486c3d1ce93f42
GenXu1 <jsycxugen@gmail.com>
2025-01-21 21:42:05 -0600

    boxplot

--- test.R
+++ test.R
@@ -3,3 +3,5 @@ for(i in 1:10)
 {cat("This is number",i,"\n")}
 
 plot(1:100,col=rainbow(100),pch=16,cex=2)
+
+boxplot(rnorm(200,mean=0,sd=1),rnorm(200,mean=5,sd=1),names=c("Grp1","Grp2"),col=c("darkgreen","orange"),las=1)
